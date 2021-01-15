33a2238ba748093bc305739ce7b54fbff345230b
Maycol <maycol320@gmail.com>
2021-01-15 15:00:36 -0300

    commit inicial

---
+++ config/yargs.js
@@ -0,0 +1,27 @@
+//tipos de requirds
+// const fs = require('express');
+// const fs = require('./fs');
+const opts = {
+    base: {
+        demand: true,
+        alias: 'b',
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+
+    }
+
+}
+
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', opts)
+    .command('crear', 'Genera un archivo con la tabla multiplicar', opts)
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+
+
+}
